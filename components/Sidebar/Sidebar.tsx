c8056077e51b6d396a42cb0adc0e9252cf0e042f
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-04-29 00:38:58 -0700

    Sidebar
     - Sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,6 +1,25 @@
-import Head from 'next/head';
-import styles from './Sidebar.module.css'
+import cx from 'classnames';
+import { FaTimes } from 'react-icons/fa';
+import { Links } from '../Links/Links';
+import { Socials } from '../Socials/Socials';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = () => {
-  return (<></>)
-}
+export const Sidebar = ({
+  isOpen,
+  toggleSidebar,
+}: {
+  isOpen: Boolean;
+  toggleSidebar: (event: React.MouseEvent<HTMLElement>) => void;
+}) => {
+  return (
+    <aside className={cx(styles.Sidebar, isOpen ? styles.ShowSidebar : '')}>
+      <button className={styles.CloseBtn} onClick={toggleSidebar}>
+        <FaTimes />
+      </button>
+      <div className={styles.SideContainer ? styles.SideContainer : ''}>
+        <Links styleClass={`${isOpen ? 'sidebarLinks' : ''}`} toggleSidebar={ toggleSidebar}/>
+        <Socials styleClass={`${isOpen ? 'sidebarIcons' : ''}`} />
+      </div>
+    </aside>
+  );
+};
