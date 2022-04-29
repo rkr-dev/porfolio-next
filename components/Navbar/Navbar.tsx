af92c7e7d214012881e641f88c86e7ed887fed19
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-04-29 00:38:25 -0700

    Navbar
     - Fixes

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,38 @@
-import Head from 'next/head';
+import cx from 'classNames';
+import Link from 'next/link';
+import { FaAlignRight } from 'react-icons/fa';
 import styles from './Navbar.module.css';
+import { Links } from '../Links/Links';
 
-export const Navbar = () => {
-  return <>
-  </>
-}
+
+export const Navbar = ({ toggleSidebar }: { toggleSidebar: (event: React.MouseEvent<HTMLElement>) => void }) => {
+  return (
+    <nav className={cx(styles.Navbar)}>
+      <div className={styles.NavCenter}>
+        <div className={styles.NavHeader}>
+          <Link href='/'>
+            <a>
+              <img
+                src='/logo.svg'
+                alt='logo'
+                style={{
+                  width: '50px',
+                  height: '50px',
+                  border: '3px solid hsl(209, 61%, 16%)',
+                }}
+              />
+            </a>
+          </Link>
+          <button
+            type='button'
+            className={styles.ToggleBtn}
+            onClick={toggleSidebar}
+          >
+            <FaAlignRight></FaAlignRight>
+          </button>
+        </div>
+        <Links styleClass='navLinks'></Links>
+      </div>
+    </nav>
+  );
+};
