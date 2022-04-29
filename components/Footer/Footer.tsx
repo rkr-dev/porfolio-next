1db9a709d29808d289069b7f1ab60bfd74f17b1f
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-04-29 00:39:23 -0700

    Footer  - Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
-import Head from 'next/head';
+import { Socials } from '../../components';
 import styles from './Footer.module.css';
 
 export const Footer = () => {
   return (
-    <>
-    </>
-  )
-}
-
+    <footer className={styles.Footer}>
+      <div>
+        <Socials styleClass='footerLinks'></Socials>
+        <h4>
+          Copyright &copy; {new Date().getFullYear()}
+          <span>RK</span> All rights reserved
+        </h4>
+      </div>
+    </footer>
+  );
+};
