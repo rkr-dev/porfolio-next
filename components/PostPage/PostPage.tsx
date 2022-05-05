1d93672c0b2f7271d9041f7579a4b7dcd3263cf7
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-05-05 04:59:54 -0700

    Post Page  - Template

---
+++ components/PostPage/PostPage.tsx
@@ -0,0 +1,39 @@
+import cx from 'classnames';
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+import Link from 'next/link';
+import { PostProps } from '../../interfaces';
+import styles from './PostPage.module.css';
+
+type PostPageProps = {
+  post: PostProps;
+};
+
+export const PostPage = ({
+  post: { id, title, content, image, slug, category, desc, date },
+}: PostPageProps) => {
+  return (
+    <section className={styles.PostPage}>
+      <Image
+        src={image}
+        width={`100%`}
+        height={`40%`}
+        layout='responsive'
+        alt={title}
+      />
+      <div className={cx('sectionCenter')}>
+        <article className={styles.PostContent}>
+          <h1>{title}</h1>
+          <div className={styles.PostDetails}>
+            <span>{ category}</span>
+            <span>{ date}</span>
+          </div>
+          <ReactMarkdown children={content} />
+        </article>
+        <Link href='/blog'>
+          <a className={cx('btn', 'centerBtn')}> blog</a>
+        </Link>
+      </div>
+    </section>
+  );
+};
