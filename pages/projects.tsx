4a94aaae3d7cf04b800bf77620d2a3e2155a478b
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-05-23 23:30:03 -0700

    Project Page
     - Project Page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,13 @@
 import type { ReactElement } from 'react';
 import { Layout, Projects } from '../components';
-import {projects } from '../utils/data'
+import { GET_ALL_PROJECTS_PROJECTS } from '../graphql';
+import { client } from '../graphql/apollo';
 
-export default function ProjectsPage() {
-  
+export default function ProjectsPage({ projects}:any) {
+
   return (
     <section>
-      <Projects projects={projects} title='All projects'/>
+      <Projects projects={projects} title='All projects' />
     </section>
   );
 }
@@ -14,3 +15,28 @@ export default function ProjectsPage() {
 ProjectsPage.getLayout = function getLayout(projectsPage: ReactElement) {
   return <Layout>{projectsPage}</Layout>;
 };
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: GET_ALL_PROJECTS_PROJECTS,
+  });
+  const allProjects = data.projects.data.map((project: any) => {
+    return {
+      id: project.id,
+      title: project.attributes.title,
+      description: project.attributes.description,
+      image: project.attributes.image.data.attributes,
+      github: project.attributes.github,
+      url: project.attributes.url,
+      featured: project.attributes.featured,
+      stack: project.attributes.stack,
+      visible: project.attributes.visible,
+    };
+  });
+
+  return {
+    props: {
+      projects: allProjects,
+    },
+  };
+}
