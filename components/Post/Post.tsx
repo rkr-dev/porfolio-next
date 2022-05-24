06021f27c49814fde9e7c1a23d19587d26b0f6f5
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-05-23 23:31:43 -0700

    Blog Page
     - Blogs Page

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { PostProps } from '../../interfaces';
 import styles from './Post.module.css';
 
 export const Post = ({
@@ -11,16 +12,7 @@ export const Post = ({
   category,
   slug,
   desc,
-}: {
-  id: string;
-  title: string;
-  content: string;
-  image: string;
-  slug: string;
-  category: string;
-  desc: string;
-  date: string;
-}) => {
+}: PostProps) => {
   return (
     <Link href={`/blog/[slug]`} as={`/blog/${slug}`} key={id}>
       <a>
@@ -28,8 +20,8 @@ export const Post = ({
           <div className={styles.BlogImg}>
             {image && (
               <Image
-                src={image}
-                alt={title}
+                src={image.url}
+                alt={image.alternativeText}
                 width={`100%`}
                 height={`100%`}
                 layout='responsive'
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,12 @@
 import type { ReactElement } from 'react';
+import { client } from '../graphql/apollo';
 import { Layout, Posts } from '../components';
-import { posts } from '../utils/data';
+import { GET_ALL_POSTS_BLOG } from '../graphql';
 
 
-export default function BlogPage() {
+export default function BlogPage({posts}:any) {
   return (
-      <section className='blogPage'>
+    <section className='blogPage'>
         <Posts posts={posts} title='Blog' />
       </section>
   );
@@ -14,3 +15,28 @@ export default function BlogPage() {
 BlogPage.getLayout = function getLayout(blogPage: ReactElement) {
   return <Layout>{blogPage}</Layout>;
 };
+
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: GET_ALL_POSTS_BLOG,
+  });
+  const allBlogs = data.blogs.data.map((post: any) => {
+    return {
+      id: post.id,
+      title: post.attributes.title,
+      content: post.attributes.content,
+      image: post.attributes.image.data.attributes,
+      slug: post.attributes.slug,
+      category: post.attributes.category,
+      desc: post.attributes.desc,
+      date: post.attributes.date,
+    };
+  });
+
+  return {
+    props: {
+      posts: allBlogs,
+    },
+  };
+}
